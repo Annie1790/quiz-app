4eb4d332192960cc86ebd32615df2a21ac34a747
Annie1790 <budayaniko@outlook.com>
2023-03-30 16:08:31 +0100

    Result can be copied to clipboard. Extra questions added, only Hungary and Portugal is missing some questions

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,7 +1,8 @@
 //categories 
 
 const questions = [
-    {   type: "france",
+    {
+        type: "france",
         question: "Which of these Bordeaux estates is NOT a 'First Growth'?",
         answers: [
             {
@@ -22,28 +23,30 @@ const questions = [
             }
         ]
     },
-    {   type: "hungary",
-    question: "How many wine regions Hungary has?",
-    answers: [
-        {
-            answer: "22",
-            correct: true,
-        },
-        {
-            answer: "15",
-            correct: false,
-        },
-        {
-            answer: "9",
-            correct: false,
-        },
-        {
-            answer: "27",
-            correct: false,
-        }
-    ]
-},
-    {   type: "france",
+    {
+        type: "hungary",
+        question: "How many wine regions Hungary has?",
+        answers: [
+            {
+                answer: "22",
+                correct: true,
+            },
+            {
+                answer: "15",
+                correct: false,
+            },
+            {
+                answer: "9",
+                correct: false,
+            },
+            {
+                answer: "27",
+                correct: false,
+            }
+        ]
+    },
+    {
+        type: "france",
         question: "Which Chateau has a second wine known as 'Le Petit Lion'?",
         answers: [
             {
@@ -64,7 +67,8 @@ const questions = [
             }
         ]
     },
-    {   type: "france",
+    {
+        type: "france",
         question: "Which were the two estates that were upgraded to Premier Grand Cru Classe A status in the St-Emilion classification in 2012?",
         answers: [
             {
@@ -85,28 +89,30 @@ const questions = [
             }
         ]
     },
-    {   type: "italy",
+    {
+        type: "italy",
         question: "Where is the wine region of Trentino?",
         answers: [
             {
-                answer: "Italy",
+                answer: "Trentino-Alto Adige",
                 correct: true,
             },
             {
-                answer: "Argentina",
+                answer: "Veneto",
                 correct: false,
             },
             {
-                answer: "France",
+                answer: "Friuli-Venezia-Giula",
                 correct: false,
             },
             {
-                answer: "Hungary",
+                answer: "Piedmont",
                 correct: false,
             }
         ]
     },
-    {   type: "france",
+    {
+        type: "france",
         question: "True or false? in Saint-Joseph, Syrah must be used excusively for wine production.",
         answers: [
             {
@@ -119,7 +125,8 @@ const questions = [
             }
         ]
     },
-    {   type: "france",
+    {
+        type: "france",
         question: "The most common grape for Cognac and Armagnac is...",
         answers: [
             {
@@ -140,7 +147,8 @@ const questions = [
             }
         ]
     },
-    {   type: "france",
+    {
+        type: "france",
         question: "Where is Fixin located?",
         answers: [
             {
@@ -161,7 +169,8 @@ const questions = [
             }
         ]
     },
-    {   type: "france",
+    {
+        type: "france",
         question: "What Burgundy Classification does the following Appellation have: 'Romanée Conti (Cote de Nuits)'",
         answers: [
             {
@@ -182,7 +191,8 @@ const questions = [
             }
         ]
     },
-    {   type: "france",
+    {
+        type: "france",
         question: "Where in France is the following located: 'Chateau Pape-Clement'",
         answers: [
             {
@@ -203,7 +213,8 @@ const questions = [
             }
         ]
     },
-    {   type: "portugal",
+    {
+        type: "portugal",
         question: "Which is the correct sort on these Port Wine Regions from west to east?",
         answers: [
             {
@@ -224,7 +235,8 @@ const questions = [
             }
         ]
     },
-    {   type: "france",
+    {
+        type: "france",
         question: "All of the following French appellations are in Bordeaux region expect for one - which is the odd one out?",
         answers: [
             {
@@ -245,7 +257,8 @@ const questions = [
             }
         ]
     },
-    {   type: "italy",
+    {
+        type: "italy",
         question: "Which grape variety used for Gavi DOCG production?",
         answers: [
             {
@@ -266,7 +279,8 @@ const questions = [
             }
         ]
     },
-    {   type: "italy",
+    {
+        type: "italy",
         question: "Which grape variety used for Chianti DOC production?",
         answers: [
             {
@@ -287,7 +301,8 @@ const questions = [
             }
         ]
     },
-    {   type: "usa",
+    {
+        type: "usa",
         question: "Which is the most widely planted black grape in California?",
         answers: [
             {
@@ -308,7 +323,8 @@ const questions = [
             }
         ]
     },
-    {   type: "usa",
+    {
+        type: "usa",
         question: "What is the most widely planted white grape in California?",
         answers: [
             {
@@ -329,7 +345,370 @@ const questions = [
             }
         ]
     },
-
+    {
+        type: "usa",
+        question: "What is the name of the mountain that seperates Oregon and Washington?",
+        answers: [
+            {
+                answer: "Cascade Mountains",
+                correct: true,
+            },
+            {
+                answer: "Rocky Mountains",
+                correct: false,
+            },
+            {
+                answer: "Massif Central",
+                correct: false,
+            },
+            {
+                answer: "Coast Ranges",
+                correct: false,
+            }
+        ]
+    },
+    {
+        type: "usa",
+        question: "Which AVA shared between Napa and Sonoma Valley?",
+        answers: [
+            {
+                answer: "Los Carneros",
+                correct: true,
+            },
+            {
+                answer: "Howell Mountain",
+                correct: false,
+            },
+            {
+                answer: "Mount Veeder",
+                correct: false,
+            },
+            {
+                answer: "St. Helena",
+                correct: false,
+            }
+        ]
+    },
+    {
+        type: "usa",
+        question: "Opus One is a joint venture between __ and __.",
+        answers: [
+            {
+                answer: "Robert Mondavi and Baron Philippe de Rotschild",
+                correct: true,
+            },
+            {
+                answer: "Robert Mondavi and Paul Champoux",
+                correct: false,
+            },
+            {
+                answer: "Charles Krug and Baron Philippe de Rotschild",
+                correct: false,
+            },
+            {
+                answer: "Andre Lurton and Paul Champoux",
+                correct: false,
+            }
+        ]
+    },
+    {
+        type: "usa",
+        question: "True or false? Screaming eagle is made of 100% cabernet sauvignon.",
+        answers: [
+            {
+                answer: "True",
+                correct: true,
+            },
+            {
+                answer: "False",
+                correct: false,
+            }
+        ]
+    },
+    {
+        type: "usa",
+        question: "On the following AVAs, which is the largest?",
+        answers: [
+            {
+                answer: "Willamette Valley, Oregon",
+                correct: false,
+            },
+            {
+                answer: "Napa Valley, California",
+                correct: false,
+            },
+            {
+                answer: "North Fork of Long Island, New York",
+                correct: true,
+            },
+            {
+                answer: "Snake River Valley, Idaho and Oregon",
+                correct: false,
+            }
+        ]
+    },
+    {
+        type: "usa",
+        question: "At the 2000s Auction Napa Valley, a 6 litre Imperial bottle of 1992 Screaming Eagle was sold for how much?",
+        answers: [
+            {
+                answer: "$500.000",
+                correct: true,
+            },
+            {
+                answer: "$100.000",
+                correct: false,
+            },
+            {
+                answer: "$1.000.000",
+                correct: false,
+            },
+            {
+                answer: "$10.000",
+                correct: false,
+            }
+        ]
+    },
+    {
+        type: "italy",
+        question: " If you were in the Italian Veneto region, drinking a local, strong, full-bodied Italian red wine, which of these Italian products might it be?",
+        answers: [
+            {
+                answer: "Amarone",
+                correct: true,
+            },
+            {
+                answer: "Amaretto",
+                correct: false,
+            },
+            {
+                answer: "Amari",
+                correct: false,
+            },
+            {
+                answer: "Amatriciana",
+                correct: false,
+            }
+        ]
+    },
+    {
+        type: "italy",
+        question: "Northeastern Italy is home to many a white wine. Which of these is a sparkling white wine produced there?",
+        answers: [
+            {
+                answer: "Prosecco",
+                correct: true,
+            },
+            {
+                answer: "Prodi",
+                correct: false,
+            },
+            {
+                answer: "Primitivo",
+                correct: false,
+            },
+            {
+                answer: "Profiterole",
+                correct: false,
+            }
+        ]
+    },
+    {
+        type: "italy",
+        question: "What is the grape variety they use in Prosecco production?",
+        answers: [
+            {
+                answer: "Glera",
+                correct: true,
+            },
+            {
+                answer: "Cortese",
+                correct: false,
+            },
+            {
+                answer: "Chardonnay",
+                correct: false,
+            },
+            {
+                answer: "Pinot Noir",
+                correct: false,
+            }
+        ]
+    },
+    {
+        type: "italy",
+        question: "Which animal is on the crest of Chianti Classico?",
+        answers: [
+            {
+                answer: "Black Rooster",
+                correct: true,
+            },
+            {
+                answer: "Red Boar",
+                correct: false,
+            },
+            {
+                answer: "Golden Goose",
+                correct: false,
+            },
+            {
+                answer: "White Horse",
+                correct: false,
+            }
+        ]
+    },
+    {
+        type: "italy",
+        question: "Where is Bolgheri DOC can be found?",
+        answers: [
+            {
+                answer: "Tuscany",
+                correct: true,
+            },
+            {
+                answer: "Lazio",
+                correct: false,
+            },
+            {
+                answer: "Marche",
+                correct: false,
+            },
+            {
+                answer: "Umbria",
+                correct: false,
+            }
+        ]
+    },
+    {
+        type: "italy",
+        question: "What is Aglianico?",
+        answers: [
+            {
+                answer: "Red grape variety",
+                correct: true,
+            },
+            {
+                answer: "The programmer of this app :)",
+                correct: true,
+            },
+            {
+                answer: "Winery in Barolo",
+                correct: false,
+            },
+            {
+                answer: "White grape variety",
+                correct: false,
+            }
+        ]
+    },
+    {
+        type: "italy",
+        question: "What does Vin Santo means?",
+        answers: [
+            {
+                answer: "Holy Wine",
+                correct: true,
+            },
+            {
+                answer: "Holy drink",
+                correct: false,
+            },
+            {
+                answer: "Catholic Wine",
+                correct: false,
+            },
+            {
+                answer: "Sacrament of Eucharist",
+                correct: false,
+            }
+        ]
+    },
+    {
+        type: "france",
+        question: "True or false? Bandol wines are based on Mourvedre.",
+        answers: [
+            {
+                answer: "True",
+                correct: true,
+            },
+            {
+                answer: "False",
+                correct: false,
+            },
+        ]
+    },
+    {
+        type: "usa",
+        question: "True or false? Lodi is famous for its chardonnay production.",
+        answers: [
+            {
+                answer: "False",
+                correct: true,
+            },
+            {
+                answer: "True",
+                correct: false,
+            },
+        ]
+    },
+    {
+        type: "usa",
+        question: "Finger Lakes AVA is famous for __ grape.",
+        answers: [
+            {
+                answer: "Riesling",
+                correct: true,
+            },
+            {
+                answer: "Chardonnay",
+                correct: false,
+            },
+            {
+                answer: "Syrah",
+                correct: false,
+            },
+            {
+                answer: "Furmint",
+                correct: false,
+            }
+        ]
+    },
+    {
+        type: "hungary",
+        question: "During the Ottoman occupation of Hungary, __ fortified the defenders of castle Eger in 1552.",
+        answers: [
+            {
+                answer: "Bull's Blood",
+                correct: true,
+            },
+            {
+                answer: "Beer",
+                correct: false,
+            },
+            {
+                answer: "Apple Juice",
+                correct: false,
+            },
+            {
+                answer: "Chicken Liver",
+                correct: false,
+            }
+        ]
+    },
+    {
+        type: "hungary",
+        question: "True or false? In a 1571 document mentioned, Louis XIV of France christened Tokaji Aszu as 'Vinum Regum, Rex Vinorum'.",
+        answers: [
+            {
+                answer: "True",
+                correct: true,
+            },
+            {
+                answer: "False",
+                correct: false,
+            }
+        ]
+    },
 ];
 
 let shuffle = array => {
--- src/components/Result.js
+++ src/components/Result.js
@@ -6,6 +6,7 @@ import "../style/Result.scss";
 import { motion } from 'framer-motion';
 
 const Result = ({ score, index }) => {
+    let finalString = "";
 
     let result = 0;
     result = (score / index) * 100;
@@ -17,22 +18,46 @@ const Result = ({ score, index }) => {
         )
     }
 
-    let copyResults = () => {
-        alert("Copied!")
-    }
+    let CongratzString = () => {
+        function between(x, min, max) {
+            return x >= min && x <= max;
+          };
+
+          if (between(result, 0, 20)) {
+            finalString = "Might be beneficial to study more!"
+          }
+          else if (between(result, 21, 40)) {
+            finalString = "You have a basic knowledge, which is good!"
+          }
+          else if (between(result, 41, 60)) {
+            finalString = "Not a bad result!"
+          }
+          else if (between(result, 61, 80)) {
+            finalString = "You really know your stuff!"
+          }
+          else if (between(result, 81, 100)) {
+            finalString = "Amazing! Congratulations!"
+          }
+
+        return (
+            <p>
+                {finalString}
+            </p>
+        )
+    };
 
     return (
         <motion.div id="resultDiv"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            >
-            <p>Congratulations!</p>
+        >
+            <CongratzString />
             <CountScorePercentage />
             <img src={wineGlass} alt="congratulations!" />
             <div>
                 <button>Start Again</button>
-                <button onClick={copyResults}>Copy results</button>
+                <button onClick={() => {navigator.clipboard.writeText(`My score in wine quiz is ${result}%`); alert("Result copied!")}}>Copy results</button>
             </div>
         </motion.div>
     )
